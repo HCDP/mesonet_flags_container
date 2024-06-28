2558ff46e7ebe5bb45501a9a5f2373850bec741e
jardmclan <mcleanj@hawaii.edu>
2024-06-28 10:09:53 -1000

    load github env with dotenv

--- update_actor.py
+++ update_actor.py
@@ -1,9 +1,13 @@
 import os
 import sys
 from tapipy.tapis import Tapis
+from dotenv import load_dotenv
+from pathlib import Path
+
+dotenv_path = Path(os.environ.get("GITHUB_ENV"))
+load_dotenv(dotenv_path = dotenv_path)
 
 actor_id = os.environ.get("ACTOR_ID")
-print(actor_id)
 tapis_client = Tapis(
     base_url = "https://dev.develop.tapis.io",
     username = os.environ.get("TAPIS_USERNAME"),
