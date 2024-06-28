6968fb941306aa652f6409b83914595be76e5055
jardmclan <mcleanj@hawaii.edu>
2024-06-28 10:16:03 -1000

    debug

--- update_actor.py
+++ update_actor.py
@@ -8,6 +8,7 @@ dotenv_path = Path(os.environ.get("GITHUB_ENV"))
 load_dotenv(dotenv_path = dotenv_path)
 
 actor_id = os.environ.get("ACTOR_ID")
+print(actor_id)
 tapis_client = Tapis(
     base_url = "https://dev.develop.tapis.io",
     username = os.environ.get("TAPIS_USERNAME"),
