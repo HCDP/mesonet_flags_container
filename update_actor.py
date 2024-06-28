75194856846a4db1f89d215710c81dcb4f13f990
jardmclan <mcleanj@hawaii.edu>
2024-06-28 10:33:08 -1000

    use command line args for updater

--- update_actor.py
+++ update_actor.py
@@ -4,14 +4,13 @@ from tapipy.tapis import Tapis
 
 [ image, actor_id, tapis_username, tapis_password ] = sys.argv[1:]
 
-print(image, actor_id)
-# tapis_client = Tapis(
-#     base_url = "https://dev.develop.tapis.io",
-#     username = os.environ.get("TAPIS_USERNAME"),
-#     password = os.environ.get("TAPIS_PASSWORD"),
-#     account_type = "user",
-#     tenant_id = "dev"
-# )
+tapis_client = Tapis(
+    base_url = "https://dev.develop.tapis.io",
+    username = tapis_username,
+    password = tapis_password,
+    account_type = "user",
+    tenant_id = "dev"
+)
 
-# tapis_client.get_tokens()
-# tapis_client.streams.update_actor(actor_id = actor_id, image = sys.argv[1])
+tapis_client.get_tokens()
+tapis_client.streams.update_actor(actor_id = actor_id, image = image)
