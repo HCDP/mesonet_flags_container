42815ffb0a63b84ea91677a1298b2ae95975221b
jardmclan <mcleanj@hawaii.edu>
2024-06-28 10:18:49 -1000

    debug

--- update_actor.py
+++ update_actor.py
@@ -10,13 +10,13 @@ print(os.environ.get("GITHUB_ENV"))
 
 actor_id = os.environ.get("ACTOR_ID")
 print(actor_id)
-tapis_client = Tapis(
-    base_url = "https://dev.develop.tapis.io",
-    username = os.environ.get("TAPIS_USERNAME"),
-    password = os.environ.get("TAPIS_PASSWORD"),
-    account_type = "user",
-    tenant_id = "dev"
-)
+# tapis_client = Tapis(
+#     base_url = "https://dev.develop.tapis.io",
+#     username = os.environ.get("TAPIS_USERNAME"),
+#     password = os.environ.get("TAPIS_PASSWORD"),
+#     account_type = "user",
+#     tenant_id = "dev"
+# )
 
-tapis_client.get_tokens()
-tapis_client.streams.update_actor(actor_id = actor_id, image = sys.argv[1])
+# tapis_client.get_tokens()
+# tapis_client.streams.update_actor(actor_id = actor_id, image = sys.argv[1])
